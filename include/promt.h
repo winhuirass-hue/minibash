1070b3ab97db3a00f92c78a4ebc8c1a65eafd444
winhuirass-hue <winhuirass@gmail.com>
2026-04-09 18:22:48 +0300

    sss

--- include/promt.h
+++ include/promt.h
@@ -7,27 +7,16 @@
 #include <sys/types.h>
 
 
-#ifdef __GLIBC__
-#include <pwd.h>
-#endif
-
-
 static void prompt(void) {
     char cwd[128];
     const char *user = "user";
 
-    /* username */
-    struct passwd *pw = getpwuid(getuid());
-    if (pw && pw->pw_name)
-        user = pw->pw_name;
 
     /* current directory */
     if (!getcwd(cwd, sizeof(cwd)))
         cwd[0] = '?', cwd[1] = 0;
 
     /* print: user@cwd */
-    out_puts(user);
-    out_putc('@');
     out_puts(cwd);
 
     /* root or user */
